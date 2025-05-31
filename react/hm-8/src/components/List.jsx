d980d258f24d9132469125a68d11d90c27c2df3a
Narizhnyi Vladyslav <vulfiontop@gmail.com>
2025-05-31 17:17:14 +0200

    add react/hm-8

---
+++ react/hm-8/src/components/List.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+
+const List = () => {
+	const [items, setItems] = useState([])
+	const [inputValue, setInputValue] = useState('')
+
+	const addItem = () => {
+		if (inputValue.trim() !== '') {
+			setItems([...items, inputValue])
+			setInputValue('')
+		}
+	} 
+
+	useEffect(() => {
+		console.log('Компонент ListItems обновлен')
+	}, [])
+
+	return (
+		<div>
+			<input
+				type='text'
+				value={inputValue}
+				onChange={event => setInputValue(event.target.value)}
+				placeholder='Введите элемент'
+			/>
+			<button onClick={addItem}>Добавить элемент</button>
+			<ul>
+				{items.map((item, index) => (
+					<li key={index}>{item}</li>
+				))}
+			</ul>
+		</div>
+	)
+}
+
+export default List
